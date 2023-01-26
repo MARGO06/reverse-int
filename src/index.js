ffd2d308d24c67db71b35ccdc8f42f20dbbc9774
Margarita Masko <margaritam996@gmail.com>
2023-01-26 18:15:33 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str= String(n);
+  let arr1= str.split('');
+ let arr2= arr1.reverse();
+ let str1= arr2.join('');
+ let num1=Number(str1);
+ return num1*(-1);
+ 
+   
 }
