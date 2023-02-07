c7178cf821a52dcc50d6e660c5416cfd33ab712b
Margarita Masko <margaritam996@gmail.com>
2023-02-07 21:42:07 +0300

    feat: the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 module.exports = function reverse (n) {
-  let str= String(n);
-  let arr1= str.split('');
- let arr2= arr1.reverse();
- let str1= arr2.join('');
- let num1=Number(str1);
- return num1*(-1);
- 
-   
+  let str = String(n);
+  let arr1 = str.split('');
+  let arr3 = arr1.reverse( );
+  let str2='';
+  if (n<0){
+    arr3.pop('');
+    return str2=arr3.join("");
+  } else {
+    return  str2 = arr3.join("");
+  }
 }
